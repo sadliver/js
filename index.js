f7c80bd838e817c36a3eef6aac01f13683c05bff
sadliver <guideji_stitchnoi@hotmail.com>
2020-02-17 14:03:47 +0700

    ex4.6

---
+++ index.js
@@ -0,0 +1,32 @@
+let ex1 = "" + 1 + 0;
+let ex2 = "" - 1 + 0;
+let ex3 = true + false;
+let ex4 = 6 / "3";
+let ex5 = "2" * "3";
+let ex6 = 4 + 5 + "px";
+let ex7 = "$" + 4 + 5;
+let ex8 = "4" - 2;
+let ex9 = "4px" - 2;
+let ex10 = 7 / 0;
+let ex11 = "  -9  " + 5;
+let ex12 = "  -9  " - 5;
+let ex13 = null + 1;
+let ex14 = undefined + 1;
+let ex15 =" \t \n" - 2;
+
+
+console.log(ex1);
+console.log(ex2);
+console.log(ex3);
+console.log(ex4);
+console.log(ex5);
+console.log(ex6);
+console.log(ex7);
+console.log(ex8);
+console.log(ex9);
+console.log(ex10);
+console.log(ex11);
+console.log(ex12);
+console.log(ex13);
+console.log(ex14);
+console.log(ex15);
